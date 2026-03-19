bd756517b8d76c16306e4a43abcba457b6d2ce86
BACK-2-15 <back-2-15@Erxess-Mac-mini.local>
2026-03-19 20:16:19 +0800

    login hiih, forgot password hiih

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,11 +1,74 @@
 import { signIn } from "@/lib/auth";
+import { prisma } from "@/lib/prisma";
+import bcrypt from "bcryptjs";
+import Nodemailer from "nodemailer";
+
+const transporter = Nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
 export default function LoginPage() {
   return (
     <div className="flex min-h-screen items-center justify-center">
       <div className="rounded-lg border p-8 shadow-md w-full max-w-sm space-y-3">
         <h1 className="text-2xl font-bold mb-6 text-center">Sign in</h1>
+        <div className="rounded-lg border p-4 shadow-2xs w-fit max-w-sm space-y-3">
+          <form
+            action={async (formData: FormData) => {
+              "use server";
+              const email = formData.get("email") as string;
+              const password = formData.get("password") as string;
 
+              const user = await prisma.user.findFirst({
+                where: { email },
+              });
+
+              if (!user) {
+                throw new Error("Email not found");
+              }
+
+              const isValid = await bcrypt.compare(password, user.password);
+
+              if (!isValid) {
+                throw new Error("Incorrect password");
+              }
+
+              await signIn("credentials", {
+                email,
+                password,
+                redirectTo: "/dashboard",
+              });
+            }}
+            className="space-y-4"
+          >
+            <input
+              name="email"
+              type="email"
+              placeholder="Email"
+              required
+              className="w-full border px-3 py-2 rounded"
+            />
+
+            <input
+              name="password"
+              type="password"
+              placeholder="Password"
+              required
+              className="w-full border px-3 py-2 rounded"
+            />
+
+            <button
+              type="submit"
+              className="w-full bg-black text-white py-2 rounded"
+            >
+              Sign in
+            </button>
+          </form>
+        </div>
         <form
           action={async () => {
             "use server";
@@ -33,7 +96,7 @@ export default function LoginPage() {
           </button>
         </form>
         <p className="text-center text-sm text-gray-500">
-          No account? {""}
+          No account? Register here! == {""}
           <a href="/register" className="text-blue-200 hover:underline">
             {" "}
             Register{" "}
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,3 @@
-
-
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { authConfig } from "@/lib/auth.config";
